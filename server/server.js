26aa5fa361b2c2ee2a410b674fd88b8199bbf15d
Lorand Kallai <lorand_kallai@epam.com>
2021-12-16 13:20:40 +0100

    express now serve br

--- server/server.js
+++ server/server.js
@@ -2,12 +2,26 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicPath = path.join(__dirname, '..', 'build');
+const buildPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 8789;
-app.use(express.static(publicPath));
+const expressStaticGzip = require('express-static-gzip');
+
+app.use(
+  '/',
+  expressStaticGzip(buildPath, {
+    enableBrotli: true,
+    customCompressions: [
+      {
+        encodingName: 'deflate',
+        fileExtension: 'zz',
+      },
+    ],
+    orderPreference: ['br'],
+  })
+);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 app.listen(port, () => {
