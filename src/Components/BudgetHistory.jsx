672bf3d0a77b6c6d5daaeb10f63630c39ae4919a
Lorand Kallai <lorandkallai@gmail.com>
2021-12-19 18:45:31 +0100

    React.memo at BudgetHistory + jest eslint plugin added

--- src/Components/BudgetHistory.jsx
+++ src/Components/BudgetHistory.jsx
@@ -1,51 +1,54 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
-import Proptypes from 'prop-types';
 import { useParams } from 'react-router-dom';
 import { Skeleton, Stack } from '@chakra-ui/react';
+import Proptypes from 'prop-types';
 import { BudgetItem } from './BudgetItem';
 
 import { selectBudgetAccount } from '../store/budget/budgetSlice';
 
-export const BudgetHistory = ({ showExpenses, showIncomes, sortBy }) => {
-  const userBudget = useSelector(selectBudgetAccount);
+export const BudgetHistory = React.memo(
+  ({ showExpenses, showIncomes, sortBy }) => {
+    const userBudget = useSelector(selectBudgetAccount);
 
-  const { account } = useParams();
-  if (userBudget[account]) {
-    const currentBudget = [...userBudget[account]];
-    return sortBy === 'date'
-      ? currentBudget
-          .sort((a, b) => b.date.localeCompare(a.date))
-          .map((budget) => (
-            <BudgetItem
-              key={budget.id}
-              budget={budget}
-              showExpenses={showExpenses}
-              showIncomes={showIncomes}
-            />
-          ))
-      : currentBudget
-          .sort((a, b) => {
-            const amountA = a.expense || a.income;
-            const amountB = b.expense || b.income;
-            return amountB - amountA;
-          })
-          .map((budget) => (
-            <BudgetItem
-              key={budget.id}
-              budget={budget}
-              showExpenses={showExpenses}
-              showIncomes={showIncomes}
-            />
-          ));
+    const { account } = useParams();
+    if (userBudget[account]) {
+      const currentBudget = [...userBudget[account]];
+      return sortBy === 'date'
+        ? currentBudget
+            .sort((a, b) => b.date.localeCompare(a.date))
+            .map((budget) => (
+              <BudgetItem
+                key={budget.id}
+                budget={budget}
+                showExpenses={showExpenses}
+                showIncomes={showIncomes}
+              />
+            ))
+        : currentBudget
+            .sort((a, b) => {
+              const amountA = a.expense || a.income;
+              const amountB = b.expense || b.income;
+              return amountB - amountA;
+            })
+            .map((budget) => (
+              <BudgetItem
+                key={budget.id}
+                budget={budget}
+                showExpenses={showExpenses}
+                showIncomes={showIncomes}
+              />
+            ));
+    }
+    return (
+      <Stack>
+        <Skeleton height="90px" />
+        <Skeleton height="90px" />
+        <Skeleton height="90px" />
+      </Stack>
+    );
   }
-  return (
-    <Stack>
-      <Skeleton height="90px" />
-      <Skeleton height="90px" />
-      <Skeleton height="90px" />
-    </Stack>
-  );
-};
+);
 
 BudgetHistory.propTypes = {
   showExpenses: Proptypes.bool.isRequired,
